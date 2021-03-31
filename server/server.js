a7be8db20acb757871d83ee926f53f520a1e2991
jeromeski <jeromegee@gmail.com>
2021-03-31 14:04:06 +0800

    Database setup

--- server/server.js
+++ server/server.js
@@ -1,12 +1,21 @@
 // const express = require('express');
 import express from 'express';
 import { readdirSync } from 'fs';
+import cors from 'cors';
+import mongoose from 'mongoose';
 const morgan = require('morgan');
 require('dotenv').config();
 
 const app = express();
 
+// db connection
+mongoose
+	.connect(process.env.DATABASE, { useUnifiedTopology: true })
+	.then(() => console.log('DB Connected'))
+	.catch((err) => console.log('DB Connection Failed : ', err));
+
 // middlewares
+app.use(cors());
 /** 
   *morgan running in dev mode
 */
