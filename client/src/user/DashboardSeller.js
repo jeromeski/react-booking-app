df5e19889bae8bc7410b6f46126f2aa4f848c067
jeromeski <jeromegee@gmail.com>
2021-04-11 10:26:47 +0800

    Conditional rendering for connected and notconnected

--- client/src/user/DashboardSeller.js
+++ client/src/user/DashboardSeller.js
@@ -1,10 +1,33 @@
 import DashboardNav from "../components/DashboardNav";
 import ConnectNav from "../components/ConnectNav";
+import {useSelector} from 'react-redux';
 import { Link } from "react-router-dom";
+import { Fragment } from 'react';
 
 const DashboardSeller = () => {
+
+  const {auth} = useSelector((state) => ({...state}))
+
+  const connected = () => (
+		<div className='container-fluid'>
+			<div className='row'>
+				<div className='col-md-10'>
+					<h2>Your Hotels</h2>
+				</div>
+				<div className='col-md-2'>
+					<Link to='/hotels/new' className='btn btn-primary'>
+						+ Add New
+					</Link>
+				</div>
+			</div>
+		</div>
+	);
+
+  const notConnected = () => (
+    <h1>Connect with stripe</h1>
+  )
   return (
-    <>
+    <Fragment>
       <div className="container-fluid bg-secondary p-5">
         <ConnectNav />
       </div>
@@ -13,19 +36,11 @@ const DashboardSeller = () => {
         <DashboardNav />
       </div>
 
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-md-10">
-            <h2>Your Hotels</h2>
-          </div>
-          <div className="col-md-2">
-            <Link to="/hotels/new" className="btn btn-primary">
-              + Add New
-            </Link>
-          </div>
-        </div>
-      </div>
-    </>
+      {
+        auth && auth.user && auth.user.stripe_seller && auth.user.stripe_seller.charges_enabled ? connected() : notConnected()
+      }
+     {/*<pre>{JSON.stringify(auth, null, 4)}</pre>*/}
+    </Fragment>
   );
 };
 
