674ed81c4f645709e13fed357c67a5f3fb106e36
jeromeski <jeromegee@gmail.com>
2021-04-12 18:23:45 +0800

    User stripe status in redux and local storage

--- server/routes/stripe.js
+++ server/routes/stripe.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { createConnectAccount } from '../controllers/stripe';
+import { createConnectAccount, getAccountStatus } from '../controllers/stripe';
 import { requireSignIn } from '../middleware';
 
 const router = express.Router();
 
 router.post('/create-connect-account', requireSignIn, createConnectAccount);
+router.post('/get-account-status', requireSignIn, getAccountStatus);
 
 module.exports = router;
